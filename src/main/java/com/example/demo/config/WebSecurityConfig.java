17fcaab03c74436525481fe6b45c3d84dff55759
hongem <hong90662@nuce.edu.vn>
2020-09-23 16:13:34 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.config;
+
+import com.example.demo.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        // Set đặt dịch vụ để tìm kiếm User trong Database.
+        // Và set đặt PasswordEncoder.
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+    }
+}
